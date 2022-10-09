248bff596a14c9ed444a3118faf78165f26518a4
yevstrikha <yevstrikha@gmail.om>
2022-10-09 13:13:00 -0400

    maths

---
+++ script/js.js
@@ -0,0 +1,17 @@
+let first = prompt('Enter first number',2);
+let second = prompt('Enter second number',2);
+
+
+first = parseInt(first);
+second = parseInt(second);
+
+let sum = (first + second);
+let subtraction = (first - second);
+let multiplication = (first * second);
+let division = (first / second);
+
+
+alert('Your sum is ' + sum);
+alert('Your difference is ' + subtraction);
+alert('Your product is ' + multiplication);
+alert('Your quotient is ' + division);
